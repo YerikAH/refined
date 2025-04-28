09dbae278151a4d6ec6d6a028842b559b32053e0
michxlmg <jhefrigasparalmonacid0@gmail.com>
2025-04-28 12:28:37 -0500

    🔧 chore: update Next.js config and add clsx, lucide-react, tailwind-merge

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["images.pexels.com", "wallpapers.com"],
+  },
 };
-
 export default nextConfig;
